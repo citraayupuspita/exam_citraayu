aead0985ec71bf79727aa188ba49acc92360a419
citraayupuspita <161399966+citraayupuspita@users.noreply.github.com>
2024-02-27 10:24:27 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'splashScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: SplashScreen(),
+    );
+
+  }
+}
